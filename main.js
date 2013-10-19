700c37c85785d90bc60b75fe17e3042a5e28d2fd
tomatoiris <tomatoiris@gmail.com>
2013-10-19 16:47:18 +0800

    fix #7

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-(function(){
-var canvas = document.getElementById('game');
-    ctx = canvas.getContext("2d");
-    ballX = Math.floor(Math.random() * 300); //0..300
-    ballY = Math.floor(Math.random() * 500);
+var gameModule = (function() {
+
+var timeoutVar,
+    counter = 0
+
+function start(){
+var canvas = document.getElementById('game'),
+    ctx = canvas.getContext("2d"),
+    ballX = Math.floor(Math.random() * 300), //0..300
+    ballY = Math.floor(Math.random() * 500),
     ballR = Math.floor(Math.random() * 100);
      
 canvas.width = 480;
 canvas.height = 320;
 
-ctx.fillStyle = 'bull';
+ctx.fillStyle = 'blue';
 ctx.beginPath();
 ctx.arc(ballX,ballY,ballR,0,Math.PI * 2,true);
 ctx.fill();
 
+if(counter >= 10){	
 
+}else{
+	timeoutVar = setTimeout(start,1000);
+	counter = counter + 1;
 
-ctx.fill();
+	console.log("Counter:" + counter);
+  }
+}
+
+return{
+	start:start
+ }
 }) ();
+
+gameModule.start();
